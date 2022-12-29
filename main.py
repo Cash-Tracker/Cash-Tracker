f14446ca0b87b8eaa1176f339b0613049eb5b3f1
anuragrao04 <raoanu2004@gmail.com>
2022-12-29 10:14:00 +0530

    added basic start function

---
+++ main.py
@@ -0,0 +1,27 @@
+from telegram.ext.updater import Updater
+from telegram.update import Update
+from telegram.ext.callbackcontext import CallbackContext
+from telegram.ext.commandhandler import CommandHandler
+from telegram.ext.messagehandler import MessageHandler
+from telegram.ext.filters import Filters
+
+try:
+    updater = Updater("5723259015:AAEgN-R-Vl4i9UYz5XLLQT1qUs-AQSsocrA",
+                    use_context=True)
+except:
+    print("Error in connecting to telegram API")
+
+
+
+def start(update: Update, context: CallbackContext):
+	update.message.reply_text(
+		"Hello There! We are BetterMoney. We will help you manage your expenses and track them. We are currently under development and will be live soon!")
+
+
+
+
+updater.dispatcher.add_handler(CommandHandler('start', start))
+
+
+
+updater.start_polling()
