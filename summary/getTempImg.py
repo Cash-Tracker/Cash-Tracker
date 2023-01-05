a1983826ceecd6ca7cd6179d32d39b51c035794f
Shreya <gurram13775@gmail.com>
2023-01-05 15:13:28 +0530

    percentage,colors added

--- summary/getTempImg.py
+++ summary/getTempImg.py
@@ -6,7 +6,20 @@ import matplotlib.pyplot as plt
 
 def getTempImg(lables, vals):
     matplotlib.use("Agg")
-    plt.pie(vals, labels = lables)
+    color = ['#47CACC','#F3ABB6','#A8E6CE','#D67280']
+  
+
+    def make_autopct(sizes):
+        def my_autopct(pct):
+            total = sum(sizes)
+            val = int(round(pct*total/100.0))
+            return '{p:.2f}%  ₹{v:d}'.format(p=pct,v=val)  # add rupee symbol
+        return my_autopct
+    plt.pie(vals, labels = lables, colors=color,autopct=make_autopct(vals) )
+    plt.axis('equal')
+    
+    
+    
     temp = tempfile.NamedTemporaryFile(suffix='.jpg')
     plt.savefig(temp, format = 'jpg')
     plt.clf()
