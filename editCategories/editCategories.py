31c38fb75cf6ecb7fbcaa7cfdd605a7fc6cb80bf
anuragrao04 <raoanu2004@gmail.com>
2023-01-18 19:35:38 +0530

    updated for new telegram API

--- editCategories/editCategories.py
+++ editCategories/editCategories.py
@@ -1,26 +1,26 @@
-from telegram.update import Update
-from telegram.ext.callbackcontext import CallbackContext
+from telegram import Update
+from telegram.ext import CallbackContext
 from telegram.ext import ConversationHandler, CommandHandler, MessageHandler
-from telegram.ext.filters import Filters
+import telegram.ext.filters as filters
 
 
 
 category = 0
 
 
-def editCategories(update: Update, context: CallbackContext):
-    update.message.reply_text("Please enter all the new categories: ")
+async def editCategories(update: Update, context: CallbackContext):
+    await update.message.reply_text("Please enter all the new categories: ")
     return category
 
 
-def askCategory(update: Update, context: CallbackContext):
+async def askCategory(update: Update, context: CallbackContext):
     categories = update.message.text
     categories = categories.split(',')
     categories = list(map(str.strip, categories))
     context.user_data['categories'] = categories
 
     categoriesString = "\n".join(categories)
-    update.message.reply_text(f'''Awesome!
+    await update.message.reply_text(f'''Awesome!
 Here are the categories you entered:
 {categoriesString}
 
@@ -29,7 +29,7 @@ They have been updated.
     return ConversationHandler.END
 
 
-def cancel(update: Update, context: CallbackContext):
+async def cancel(update: Update, context: CallbackContext):
     pass
 
 
@@ -41,7 +41,7 @@ def cancel(update: Update, context: CallbackContext):
 conv_handler_editCategories = ConversationHandler(
     entry_points=[CommandHandler("editcategories", editCategories)],
     states={
-        category: [MessageHandler(~Filters.command, askCategory)],
+        category: [MessageHandler(~filters.COMMAND, askCategory)],
     },
     fallbacks=[CommandHandler("cancel", cancel)],
 )
--- tempCodeRunnerFile.py
+++
@@ -1,1 +0,0 @@
-conv_handler_spent
