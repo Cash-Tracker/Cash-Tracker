443f2ef1b0e4b44e672c7f5ec43d13087c5e0740
Amogh Kulkarni <65953251+amoghbk@users.noreply.github.com>
2023-10-14 02:11:20 +0530

    Referencing issue #9

--- database/database.py
+++ database/database.py
@@ -1,18 +1,28 @@
 from database.db_init import dbCursor, db
 
 
-
 def getSummary(year, month, ID) -> list:
-    dbCursor.execute(f"select amount, category from spent where month(datetime) = {month} and year(datetime) = {year} and userID = '{ID}'")
+    dbCursor.execute(
+        "SELECT amount, category FROM spent WHERE month(datetime) = %(month)s and year(datetime) = %(year)s and userID = %(ID)s",
+        {"month": month, "year": year, "ID": ID},
+    )
     return dbCursor.fetchall()
 
 
-def pushSpent(ID, amount, category, onWhat, note, datetime) -> None: 
-    dbCursor.execute(f"insert into spent values('{ID}', {amount}, '{category}', '{onWhat}', '{note}', '{datetime}')")
+def pushSpent(ID, amount, category, onWhat, note, datetime) -> None:
+    dbCursor.execute(
+        "INSERT INTO spent VALUES(%(ID)s, %(amount)s, %(category)s, %(onWhat)s, %(note)s, %(datetime)s)",
+        {
+            "ID": ID,
+            "amount": amount,
+            "category": category,
+            "onWhat": onWhat,
+            "note": note,
+            "datetime": datetime,
+        },
+    )
     db.commit()
 
 
 # pushSpent(2, 200, 'food', 'BunSamosax2', 'IDK', "2022-12-15 20:00:00")
 # print(getSummary("2022", "12", 2))
-
-
