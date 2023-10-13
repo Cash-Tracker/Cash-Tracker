e098da53623041d4589f6fe527cd2798cec3074e
Keeru-2005 <keeru0000@gmail.com>
2023-10-14 04:13:58 +0530

    #9 Perform SQL Sanitisation on Database Queries

--- database/database.py
+++ database/database.py
@@ -3,12 +3,12 @@ from database.db_init import dbCursor, db
 
 
 def getSummary(year, month, ID) -> list:
-    dbCursor.execute(f"select amount, category from spent where month(datetime) = {month} and year(datetime) = {year} and userID = '{ID}'")
+    dbCursor.execute(f"select amount, category from spent where month(datetime) = %s and year(datetime) = %s and userID = %s",(month, year, id ))
     return dbCursor.fetchall()
 
 
 def pushSpent(ID, amount, category, onWhat, note, datetime) -> None: 
-    dbCursor.execute(f"insert into spent values('{ID}', {amount}, '{category}', '{onWhat}', '{note}', '{datetime}')")
+    dbCursor.execute(f"insert into spent values(%s,%s,%s,%s,%s,%s,%s)",(id, amount, category, onwhat, note, datetime))
     db.commit()
 
 
