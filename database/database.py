0f02acc38c73fa5ba0311c502b6bae384219e195
Rohan123R <rohan.18.2004@gmail.com>
2023-10-14 01:34:44 +0530

    Performed sanitisation on the database.py file

--- database/database.py
+++ database/database.py
@@ -1,18 +1,15 @@
 from database.db_init import dbCursor, db
 
-
-
 def getSummary(year, month, ID) -> list:
-    dbCursor.execute(f"select amount, category from spent where month(datetime) = {month} and year(datetime) = {year} and userID = '{ID}'")
+    query = "SELECT amount, category FROM spent WHERE strftime('%m', datetime) = ? AND strftime('%Y', datetime) = ? AND userID = ?"
+    dbCursor.execute(query, (month, year, ID))
     return dbCursor.fetchall()
 
-
-def pushSpent(ID, amount, category, onWhat, note, datetime) -> None: 
-    dbCursor.execute(f"insert into spent values('{ID}', {amount}, '{category}', '{onWhat}', '{note}', '{datetime}')")
+def pushSpent(ID, amount, category, onWhat, note, datetime) -> None:
+    query = "INSERT INTO spent (userID, amount, category, onWhat, note, datetime) VALUES (?, ?, ?, ?, ?, ?)"
+    dbCursor.execute(query, (ID, amount, category, onWhat, note, datetime))
     db.commit()
 
-
+# Example usage:
 # pushSpent(2, 200, 'food', 'BunSamosax2', 'IDK', "2022-12-15 20:00:00")
 # print(getSummary("2022", "12", 2))
-
-
